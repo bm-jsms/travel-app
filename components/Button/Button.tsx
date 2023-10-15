cc48700f6b46c810cbb7ab6bdccacc07dfe14342
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-10-14 19:15:04 -0500

    Navbar

---
+++ components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ButtonProps } from './Button.type';
+import Image from 'next/image';
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+	return (
+		<button
+			type={type}
+			className={`flexCenter gap-3 rounded-full border ${variant}`}
+		>
+			{icon && <Image src={icon} alt={title} width={24} height={24} />}
+			<label className='bold-16 whitespace-nowrap cursor-pointer'>
+				{title}
+			</label>
+		</button>
+	);
+};
+
+export default Button;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { NAV_LINKS } from '@/constants';
+import Button from '../Button/Button';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className='flexBetween max-container padding-container relative z-30 py-5 bg-slate-200'>
+			<Link href='/'>
+				<Image src='/hilink-logo.svg' alt='logo' width={74} height={29} />
+			</Link>
+			<ul className='hidden h-full gap-12 lg:flex'>
+				{NAV_LINKS.map(({ href, key, label }) => (
+					<Link
+						href={href}
+						key={key}
+						className='regular-16 text-black flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'
+					>
+						{label}
+					</Link>
+				))}
+			</ul>
+			<div className='lg:flexCenter hidden'>
+				<Button
+					type='button'
+					title='Log in'
+					icon='/user.svg'
+					variant='btn_dark_green'
+				/>
+			</div>
+			<Image
+				src='menu.svg'
+				alt='menu'
+				width={32}
+				height={32}
+				className='inline-block cursor-pointer lg:hidden'
+			/>
+		</nav>
+	);
 };
 
 export default Navbar;
